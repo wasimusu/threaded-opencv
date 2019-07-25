e81ca27ce71feb1d8d6c9044c4a4dd69bf99c0cf
wasimusu <wasim.khan@aggiemail.usu.edu>
2019-07-25 15:09:35 -0600

    Different ways to do parallel split and merge technique

--- parallel-ways.cpp
+++ parallel-ways.cpp
@@ -16,19 +16,48 @@
 //using namespace cv;
 //using namespace std;
 
-void someOp(cv::Mat& image, cv::Mat& output){
+void someOp(cv::Mat &image, cv::Mat &output) {
     // Not all operations are suitable for split and merge technique.
     // Like those that change the size of output or number of channels or other things that require global context like normalization.
     // multiply.
     // resize and rotate will require smart merging.
 
-    cv::rotate(output, output, 1);
+    cv::rotate(image, output, 1);
     image.convertTo(output, CV_32FC3, 10);
-//    cv::multiply(output, output, output);
     output.convertTo(output, CV_16UC3, 0.10);
 }
 
-void parallelold(cv::Mat& input, cv::Mat& output) {
+class ParallelizeOp : public cv::ParallelLoopBody {
+private:
+    cv::Mat input;
+    cv::Mat output;
+    const int num_threads = 4;
+public:
+    ParallelizeOp(cv::Mat &input, cv::Mat &output) :
+            input(input), output(output) {}
+
+    virtual void operator()(const cv::Range &range) const {
+//    virtual void operator()(std::function<void(cv::Mat &, cv::Mat &)> func) const{
+        int rows = input.rows;
+        int cols = input.cols;
+        std::cout<<"Input : "<<rows<<" , "<<cols<<"\n";
+
+        cv::Rect r1 = cv::Rect(0, 0, cols / 2, rows / 2);
+        cv::Rect r2 = cv::Rect(cols / 2, 0, cols / 2, rows / 2);
+        cv::Rect r3 = cv::Rect(0, rows / 2, cols / 2, rows / 2);
+        cv::Rect r4 = cv::Rect(cols / 2, rows / 2, cols / 2, rows / 2);
+        std::vector <cv::Rect> rois{r1, r2, r3, r4};
+
+//        for(int i = 0; i < num_threads; i++){
+        for (int i = range.start; i < range.end; i++) {
+            cv::Mat in(input, rois[i]);
+            cv::Mat out(output, rois[3 - i]);
+            someOp(std::ref(in), std::ref(out)); // Works perfectly well.
+        }
+    }
+};
+
+void parallelold(cv::Mat &input, cv::Mat &output) {
     int rows = input.rows;
     int cols = input.cols;
 
@@ -36,21 +65,20 @@ void parallelold(cv::Mat& input, cv::Mat& output) {
     cv::Rect r2 = cv::Rect(cols / 2, 0, cols / 2, rows / 2);
     cv::Rect r3 = cv::Rect(0, rows / 2, cols / 2, rows / 2);
     cv::Rect r4 = cv::Rect(cols / 2, rows / 2, cols / 2, rows / 2);
-    std::vector<cv::Rect> rois{r1, r2, r3, r4};
-
-    // Split the tasks into several threads
-    std::vector<std::thread> threads;
+    std::vector <cv::Rect> rois{r1, r2, r3, r4};
 
     // Split into rois
     cv::Mat im0 = input(rois[0]);
     cv::Mat im1 = input(rois[1]);
     cv::Mat im2 = input(rois[2]);
     cv::Mat im3 = input(rois[3]);
-    std::vector<cv::Mat> inputs = {im0, im1, im2, im3};
+    std::vector <cv::Mat> inputs = {im0, im1, im2, im3};
     cv::Mat o1, o2, o3, o4;
-    std::vector<cv::Mat> outputs = {o1, o2, o3, o4};
+    std::vector <cv::Mat> outputs = {o1, o2, o3, o4};
     output = input.clone();
 
+    // Split the tasks into several threads
+    std::vector <std::thread> threads;
     for (int i = 0; i < rois.size(); i++) {
         threads.push_back(std::thread(someOp, std::ref(inputs[i]), std::ref(outputs[i])));
     }
@@ -60,11 +88,11 @@ void parallelold(cv::Mat& input, cv::Mat& output) {
     // Smartly waiting for threads to complete their jobs.
     for (int i = 0; i < threads.size(); i++) {
         threads[i].join();
-        outputs[i].copyTo(output(rois[3-i])); // Merge it smartly to allow rotation and resizing as well.
+        outputs[i].copyTo(output(rois[3 - i])); // Merge it smartly to allow rotation and resizing as well.
     }
 }
 
-void parallel(cv::Mat& input, cv::Mat& output) {
+void parallel(cv::Mat &input, cv::Mat &output) {
     // Parallelize without having to copy at the end. Copying can be huge overhead.
 
     int rows = input.rows;
@@ -74,26 +102,24 @@ void parallel(cv::Mat& input, cv::Mat& output) {
     cv::Rect r2 = cv::Rect(cols / 2, 0, cols / 2, rows / 2);
     cv::Rect r3 = cv::Rect(0, rows / 2, cols / 2, rows / 2);
     cv::Rect r4 = cv::Rect(cols / 2, rows / 2, cols / 2, rows / 2);
-    std::vector<cv::Rect> rois{r1, r2, r3, r4};
+    std::vector <cv::Rect> rois{r1, r2, r3, r4};
 
     // Split the tasks into several threads
-    std::vector<std::thread> threads;
+    std::vector <std::thread> threads;
     output = input.clone();
     for (int i = 0; i < rois.size(); i++) {
         cv::Mat in(input, rois[i]);
-        cv::Mat out(output, rois[3-1]);
+        cv::Mat out(output, rois[3 - 1]);
         threads.push_back(std::thread(someOp, std::ref(in), std::ref(out)));
     }
 
     // Wait for all the threads to finish their task
-    // Smartly waiting for threads to complete their jobs.
-    for (int i = 0; i < threads.size(); i++) {
-        threads[i].join();
-    }
+    for (int i = 0; i < threads.size(); i++) { threads[i].join(); }
+    threads.clear();
 }
 
 
-void serial(cv::Mat& image, cv::Mat& output){
+void serial(cv::Mat &image, cv::Mat &output) {
     someOp(image, output);
 }
 
@@ -103,17 +129,18 @@ int main(int argc, char **argv) {
     cv::Mat parallelIm;
     cv::Mat serialIm;
     cv::Mat parallelNew;
+    cv::Mat parallelfor;
 
     int N = 5; // Multiple of 5
     for (int i = 0; i < N; i++) {
         // The number of cores is the optimal number of threads
         // It takes 1 ms to create and destroy 10 threads
 
-
         auto start2 = std::chrono::system_clock::now();
         parallelold(image, parallelIm);
         auto stop2 = std::chrono::system_clock::now();
-        std::cout << "Parallel Old took " << std::chrono::duration_cast<std::chrono::milliseconds>(stop2 - start2).count()
+        std::cout << "Parallel Old took "
+                  << std::chrono::duration_cast<std::chrono::milliseconds>(stop2 - start2).count()
                   << " ms\n";
 
         auto start = std::chrono::system_clock::now();
@@ -122,13 +149,21 @@ int main(int argc, char **argv) {
         std::cout << "Parallel took " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count()
                   << " ms\n";
 
+//        auto start3 = std::chrono::system_clock::now();
+////        cv::parallel_for_(someOp, ParallelizeOp(image, parallelfor));
+//        cv::parallel_for_(cv::Range(0, 4), ParallelizeOp(std::ref(image), std::ref(parallelfor)));
+//        auto stop3 = std::chrono::system_clock::now();
+//        std::cout << "parallel_for_ took "
+//                  << std::chrono::duration_cast<std::chrono::milliseconds>(stop3 - start3).count()
+//                  << " ms\n";
+
         auto start1 = std::chrono::system_clock::now();
         serial(image, serialIm);
         auto stop1 = std::chrono::system_clock::now();
         std::cout << "Serial took " << std::chrono::duration_cast<std::chrono::milliseconds>(stop1 - start1).count()
                   << " ms\n";
 
-        std::cout<<std::endl;
+        std::cout << std::endl;
     }
 
     cv::imwrite("Parallel.tif", parallelNew);
